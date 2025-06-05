33d5d293ad32dcafd7f008c5e64ad887d7e68cdd
JavadEsmati <Javad.esmatib@Gmail.com>
2025-06-05 12:36:45 +0200

    change fastify

--- api/index.js
+++ api/index.js
@@ -4,13 +4,14 @@ import fastifyStatic from '@fastify/static';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { createClient } from '@libsql/client';
-
-
-const isProd = process.env.NODE_ENV === 'production';
+import 'pino-pretty';            
 
 const app = fastify({
-  logger: true,
-})
+  logger: {
+    transport: { target: 'pino-pretty' }
+  }
+});
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
@@ -31,59 +32,71 @@ const db = {
   run: async (sql, args = []) =>  turso.execute({ sql, args }),
 };
 
-app.addHook('preHandler', (req, res, done) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST");
-  res.header("Access-Control-Allow-Headers",  "*");
-
-const isPreflight = /options/i.test(req.method);
-if (isPreflight) {
-  return res.send();
-}
-done();
-})
 
 app.get('/', async (req, res) => {
   return res.status(200).type('text/html').send(html)
 })
 app.get('/api', () => ({ status: 'SliceMeUp API is live' }));
 
-app.get("/api/pizzas", async function getPizzas(req, res) {
-  const pizzasPromise = db.all(
-    "SELECT pizza_type_id, name, category, ingredients as description FROM pizza_types"
+app.get('/pizzas', async () => {
+  const pizzas        = await db.all(
+    'SELECT pizza_type_id, name, category, ingredients AS description FROM pizza_types'
   );
-  const pizzaSizesPromise = db.all(
-    `SELECT 
-      pizza_type_id as id, size, price
-    FROM 
-      pizzas
-  `
+  const pizzaSizes    = await db.all(
+    `SELECT pizza_type_id AS id, size, price FROM pizzas`
   );
 
-  const [pizzas, pizzaSizes] = await Promise.all([
-    pizzasPromise,
-    pizzaSizesPromise,
-  ]);
-
-  const responsePizzas = pizzas.map((pizza) => {
-    const sizes = pizzaSizes.reduce((acc, current) => {
-      if (current.id === pizza.pizza_type_id) {
-        acc[current.size] = +current.price;
-      }
+  return pizzas.map(p => {
+    const sizes = pizzaSizes.reduce((acc, cur) => {
+      if (cur.id === p.pizza_type_id) acc[cur.size] = +cur.price;
       return acc;
     }, {});
     return {
-      id: pizza.pizza_type_id,
-      name: pizza.name,
-      category: pizza.category,
-      description: pizza.description,
-      image: `/pizzas/${pizza.pizza_type_id}.webp`,
+      id:          p.pizza_type_id,
+      name:        p.name,
+      category:    p.category,
+      description: p.description,
+      image:       `/public/pizzas/${p.pizza_type_id}.webp`,
       sizes,
     };
   });
-
-  res.send(responsePizzas);
 });
+// app.get("/api/pizzas", async function getPizzas(req, res) {
+//   const pizzasPromise = db.all(
+//     "SELECT pizza_type_id, name, category, ingredients as description FROM pizza_types"
+//   );
+//   const pizzaSizesPromise = db.all(
+//     `SELECT 
+//       pizza_type_id as id, size, price
+//     FROM 
+//       pizzas
+//   `
+//   );
+
+//   const [pizzas, pizzaSizes] = await Promise.all([
+//     pizzasPromise,
+//     pizzaSizesPromise,
+//   ]);
+
+//   const responsePizzas = pizzas.map((pizza) => {
+//     const sizes = pizzaSizes.reduce((acc, current) => {
+//       if (current.id === pizza.pizza_type_id) {
+//         acc[current.size] = +current.price;
+//       }
+//       return acc;
+//     }, {});
+//     return {
+//       id: pizza.pizza_type_id,
+//       name: pizza.name,
+//       category: pizza.category,
+//       description: pizza.description,
+//       image: `/pizzas/${pizza.pizza_type_id}.webp`,
+//       sizes,
+//     };
+//   });
+
+//   res.send(responsePizzas);
+// });
 
 app.get('/api/pizza-of-the-day', async () => {
   const pizzas = await db.all(`
